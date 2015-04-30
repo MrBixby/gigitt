f5e9e9f8bf25a68f0f0226b2af3ac70e66f8d8d4
Justin Doshay <justin@doshay.net>
2015-04-30 13:46:35 -0700

    mailer fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options[:host] = 'gigitt.herokuapp.com'
+  Rails.application.routes.default_url_options[:host] = 'gigitt.herokuapp.com'
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
@@ -87,6 +89,5 @@ Rails.application.configure do
    :authentication       => "plain",
   :enable_starttls_auto => true
 }
-  config.action_mailer.default_url_options[:host] = 'gigitt.herokuapp.com'
 
 end
