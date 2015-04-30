4117ac317a347f1dcf5ffb4e0405cae735fa95ef
Justin Doshay <justin@doshay.net>
2015-04-30 15:18:52 -0700

    still working on mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,18 +77,22 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'gigitt.herokuapp.com' }
+  config.action_mailer.default_url_options[:host] = 'gigitt.herokuapp.com'
   Rails.application.routes.default_url_options[:host] = 'gigitt.herokuapp.com'
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
-   :authentication       => "plain",
-  :enable_starttls_auto => true
+   address:              => "smtp.gmail.com",
+   port:                 => 587,
+   domain: ENV["GMAIL_DOMAIN"],
+   authentication:       => "plain",
+   enable_starttls_auto: => true,
+   user_name:            => ENV['gmail_username'],
+   password:             => ENV['gmail_password']
 }
 
 end
