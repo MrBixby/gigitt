2c1968eb42dbe83c8b9d5f05d86f7ce9a36d4171
Justin Doshay <justin@doshay.net>
2015-04-30 13:25:50 -0700

    still working on mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,5 @@ Rails.application.configure do
    :authentication       => "plain",
   :enable_starttls_auto => true
 }
-  Rails.application.routes.default_url_options[:host] = 'gigitt.herokuapp.com'
 
 end
