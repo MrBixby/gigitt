e6b1f68cbb649453648ad28b5273c683bf745bbd
Justin Doshay <justin@doshay.net>
2015-04-30 12:33:17 -0700

    fixed mailer not redirecting properly

--- test/mailers/gigitt_mailer_test.rb
+++ test/mailers/gigitt_mailer_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class GigittMailerTest < ActionMailer::Preview
+  Rails.application.routes.default_url_options[:host] = 'gigitt.com'
   def gigitt_mailer_preview
     gigitt_email(User.first)
   end
