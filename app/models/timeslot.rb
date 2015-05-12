b4026bc04d5b59243ae2a6ff94b42bde4c793a1d
Justin Doshay <justin@doshay.net>
2015-05-12 13:54:27 -0700

    fixed error with timeslot

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,5 +1,5 @@
 class Timeslot < ActiveRecord::Base
   belongs_to :venue
-  has_many :gigrequest //to keep it from conflicting with http request/response stuff.
+  has_many :gigrequest
   has_many :bands, through: :gigs
 end
