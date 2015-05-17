02ed00e78e0622673a22881ec70cca2125ecfcb1
Justin Doshay <justin@doshay.net>
2015-05-16 18:02:07 -0700

    created venue model and started on venue views

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -2,4 +2,10 @@ class Timeslot < ActiveRecord::Base
   belongs_to :venue
   has_many :gigrequests
   has_one :gig
+
+  validates :date, presence: true
+
+  validates :start_time, presence: true
+
+  validates :end_time, presence: true
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -3,9 +3,31 @@ class Venue < ActiveRecord::Base
   has_and_belongs_to_many :bands
   has_and_belongs_to_many :users
   has_many :timeslots, dependent: :destroy
+
+  validates :name, presence: true,
+                      uniqueness: true,
+            length: { maximum: 60 }
+
   def upcoming_gigs
-    gigs = []
     openings = self.timeslots.where('date >= ?', Date.today)
+    gig_list(openings)
+  end
+  def past_gigs
+    openings = self.timeslots.where('date <= ?', 1.week.ago)
+    gig_list(openings)
+  end
+  def present_gigs
+    openings = self.timeslots.where('date >= ? AND date <= ?', 1.week.ago, 1.week.from_now)
+    gig_list(openings)
+  end
+  def future_gigs
+    openings = self.timeslots.where('date <= ?', 1.week.from_now)
+    gig_list(openings)
+  end
+
+  private
+  def gig_list(openings)
+    gigs = []
     openings.each do |t|
       gigs << t.gig
     end
