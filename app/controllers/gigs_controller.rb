cc4821a425f21e80ec89eea9b17cd7caddd9ac46
Richard Glover <richard@gloverdesigns.com>
2015-05-28 20:59:06 -0700

    Move functionality from timeslots_controller to gigs_controller
    Signed-off-by: Richard Glover <richard@gloverdesigns.com>

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -2,24 +2,22 @@ class GigsController < ApplicationController
   respond_to :html, :json
 
   before_filter :authenticate_user!, only: [:new, :create, :edit, :destroy, :update]
-  before_action :set_gig, only: [:show, :edit, :update, :destroy]
+  before_action :set_venue, only: [:index, :new, :create, :edit, :update]
 
   def index
-    @gigs = Gig.all
+    @gigs = @venue.gigs.all
   end
 
   def show
+    @gig = Gig.find(params[:id])
   end
 
   def new
-    @timeslot = Timeslot.find(params[:timeslot_id])
-    @requests = @timeslot.gigrequests.all
-    @gig = @timeslot.build_gig
+    @gig = @venue.gig.new
   end
 
   def create
-    @gig = Gig.new(gig_params)
-    @gig.user_id = current_user.id
+    @gig = @venue.gig.new(gig_params)
 
     respond_to do |format|
       if @gig.save
@@ -33,9 +31,12 @@ class GigsController < ApplicationController
   end
 
   def edit
+    @gig = @venue.gigs.find(params[:id])
+    @bandlists = @gig.bandlists.all
   end
 
   def update
+    @gig = @venue.gigs.find(params[:id])
     respond_to do |format|
       if @gig.update(gig_params)
         format.html { redirect_to @gig, notice: 'Gig was successfully updated.' }
@@ -47,7 +48,22 @@ class GigsController < ApplicationController
     end
   end
 
+  def finalize
+    @gig = Gig.find(params[:id])
+    @gig.is_final = true
+    respond_to do |format|
+      if @gig.save
+        format.html { redirect_to dashboard_path, notice: 'Gig was finalized.'}
+        format.json { render :show, status: :ok, location: @gig }
+      else
+        format.html { render :edit }
+        format.json { head :no_content }
+      end
+    end
+  end
+
   def destroy
+    @gig = @venue.gigs.find(params[:id])
     @gig.destroy
     respond_to do |format|
       format.html { redirect_to gigs_url, notice: 'Gig was successfully destroyed.' }
@@ -56,11 +72,14 @@ class GigsController < ApplicationController
   end
 
   private
-    def set_gig
-      @gig = Gig.find(params[:id])
+    def set_venue
+      @venue = Venue.find(params[:venue_id])
     end
 
     def gig_params
-      params.require(:gig).permit(:venue, :event, :date, :band, :doors, :showtime, :age, :price, :description)
+      params.require(:gig).permit(:date, :showtime, :doors, :openings,
+        :pay_amount, :paid_gig, :notes, :venue_id, :event, :age, :price, 
+        :description, :is_final, :band_ids =>[],
+        bandlist_attributes: [:hired])
     end
 end
