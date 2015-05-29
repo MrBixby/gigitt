16c23e05c4a28326140734eb06ea3b999b29640a
Richard Glover <richard@gloverdesigns.com>
2015-05-28 20:42:25 -0700

    commented out seeds file.
    Old seeds file was causing some issues with creating a database. I commented out the seeds for now.
    
    Signed-off-by: Richard Glover <richard@gloverdesigns.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,74 +6,74 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-DEFAULT_INSECURE_PASSWORD = 'nomorebs'
+# DEFAULT_INSECURE_PASSWORD = 'nomorebs'
 
-user = User.create({
-  profile: "Justin",
-  email: "jdoshay@gigitt.com",
-  password: DEFAULT_INSECURE_PASSWORD,
-  password_confirmation: DEFAULT_INSECURE_PASSWORD
-})
+# user = User.create({
+#   profile: "Justin",
+#   email: "jdoshay@gigitt.com",
+#   password: DEFAULT_INSECURE_PASSWORD,
+#   password_confirmation: DEFAULT_INSECURE_PASSWORD
+# })
 
-venue = Venue.create({
-  name: "Chain Reaction",
-  user_id: user.id
-})
+# venue = Venue.create({
+#   name: "Chain Reaction",
+#   user_id: user.id
+# })
 
-slot1 = venue.timeslots.create({
-  date: 1.week.from_now,
-  start_time: "07:00 PM".to_time,
-  end_time: "09:00 PM".to_time,
-  openings: 2,
-  pay_amount: 100.00,
-  paid_gig: true,
-  notes: "Looking for great Punk Rock"
-})
+# slot1 = venue.timeslots.create({
+#   date: 1.week.from_now,
+#   start_time: "07:00 PM".to_time,
+#   end_time: "09:00 PM".to_time,
+#   openings: 2,
+#   pay_amount: 100.00,
+#   paid_gig: true,
+#   notes: "Looking for great Punk Rock"
+# })
 
-slot2 = venue.timeslots.create({
-  date: 1.week.ago,
-  start_time: "07:00 PM".to_time,
-  end_time: "09:00 PM".to_time,
-  openings: 2,
-  pay_amount: 100.00,
-  paid_gig: true,
-  notes: "Looking for great Punk Rock"
-})
+# slot2 = venue.timeslots.create({
+#   date: 1.week.ago,
+#   start_time: "07:00 PM".to_time,
+#   end_time: "09:00 PM".to_time,
+#   openings: 2,
+#   pay_amount: 100.00,
+#   paid_gig: true,
+#   notes: "Looking for great Punk Rock"
+# })
 
-slot3 = venue.timeslots.create({
-  date: 2.weeks.from_now,
-  start_time: "07:00 PM".to_time,
-  end_time: "09:00 PM".to_time,
-  openings: 2,
-  pay_amount: 100.00,
-  paid_gig: true,
-  notes: "Looking for EDM DJ's"
-})
+# slot3 = venue.timeslots.create({
+#   date: 2.weeks.from_now,
+#   start_time: "07:00 PM".to_time,
+#   end_time: "09:00 PM".to_time,
+#   openings: 2,
+#   pay_amount: 100.00,
+#   paid_gig: true,
+#   notes: "Looking for EDM DJ's"
+# })
 
-gig1 = slot1.create_gig({
-  event: "Punk it up",
-  doors: "06:00 PM".to_time,
-  showtime: "07:00 PM".to_time,
-  age: "21 and Up",
-  price: "$15 presale, $20 at the door",
-  description: "Punk rock rules!"
-})
+# gig1 = slot1.create_gig({
+#   event: "Punk it up",
+#   doors: "06:00 PM".to_time,
+#   showtime: "07:00 PM".to_time,
+#   age: "21 and Up",
+#   price: "$15 presale, $20 at the door",
+#   description: "Punk rock rules!"
+# })
 
-gig2 = slot2.create_gig({
-  event: "Punk it up",
-  doors: "06:00 PM".to_time,
-  showtime: "07:00 PM".to_time,
-  age: "21 and Up",
-  price: "$15 presale, $20 at the door",
-  description: "Punk rock rules!"
-})
+# gig2 = slot2.create_gig({
+#   event: "Punk it up",
+#   doors: "06:00 PM".to_time,
+#   showtime: "07:00 PM".to_time,
+#   age: "21 and Up",
+#   price: "$15 presale, $20 at the door",
+#   description: "Punk rock rules!"
+# })
 
 
 
-justin = User.find_by_email('jdoshay@gigitt.com')
-ha   = User.find_by_email('hdoshay@gigitt.com')
+# justin = User.find_by_email('jdoshay@gigitt.com')
+# ha   = User.find_by_email('hdoshay@gigitt.com')
 
-seed_user = justin
+# seed_user = justin
 
 #seed_user.gigs.create(event: "Gigitt Launch Party", date: "06-01-2015", band: "thrice", doors: "19:00", showtime: "21:00", age: "21+", price: "8", description: "Rock Your Heart Out!")
 #ha.gigs.create(event: "4th of July Festival", date: "07-04-2015", band: "pennywise", doors: "13:00", showtime: "21:00", age: "All Ages", price: "31", description: "Going To Be A Blast!")
