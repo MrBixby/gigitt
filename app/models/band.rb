58e9df42abe3bc73dea3d5f4f1312c055966ada1
Richard Glover <richard@gloverdesigns.com>
2015-05-28 20:47:17 -0700

    Edited band relationship to remove relationship with now defunct Timeslot model.
    Signed-off-by: Richard Glover <richard@gloverdesigns.com>

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,9 +1,8 @@
 class Band < ActiveRecord::Base
 
   has_and_belongs_to_many :users
-  has_many :gigrequests
-  has_many :timeslots, through: :gigrequests
-  has_and_belongs_to_many :gigs, counter_cache: :bands_count
+  has_many :bandlists
+  has_many :gigs, through: :bandlists
   has_many :fans
 
   acts_as_follower
@@ -16,7 +15,7 @@ class Band < ActiveRecord::Base
 
   def available_gigs
     self.followees(Venue).each do |venue|
-      return venue.timeslots.unfilled.upcoming.pending
+      return venue.gigs.unfilled.upcoming.pending
     end
   end
 
