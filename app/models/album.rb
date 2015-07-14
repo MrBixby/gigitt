5360fd915482126d925427d5088bc41ea237fbc4
Justin Doshay <jdoshay@gigitt.com>
2015-07-14 15:57:49 -0700

    working on band setlist
    utilizing nested models
    
    Signed-off-by: Justin Doshay <jdoshay@gigitt.com>

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,6 @@
 class Album < ActiveRecord::Base
 
     belongs_to :band
-    has_many :songs, dependent: destroy
+    has_many :songs, dependent: :destroy
     accepts_nested_attributes_for :songs, allow_destroy: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150702052045) do
+ActiveRecord::Schema.define(version: 20150714204158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150702052045) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.date     "release_year"
+    t.integer  "band_id"
   end
 
   create_table "bandlists", force: :cascade do |t|
